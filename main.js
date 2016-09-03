e34872e9f88e1de32c4856eba8ebb90a1790d5f9
Betsegaw Tadele <betsegaw.ta@gmail.com>
2016-09-03 03:47:52 -0700

    Initial class definitions

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 /// <reference path="DefinitelyTyped\createjs\createjs.d.ts" />
 function init() {
     var stage = new createjs.Stage("demoCanvas");
-    var circle = new createjs.Shape();
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-    circle.x = 100;
-    circle.y = 100;
-    stage.addChild(circle);
+    var rectangle = new createjs.Shape();
+    rectangle.graphics.beginFill("DeepSkyBlue").drawRoundRect(0, 0, 100, 100, 2);
+    rectangle.x = 100;
+    rectangle.y = 100;
+    stage.addChild(rectangle);
     stage.update();
 }
+var Block = (function () {
+    function Block(location) {
+        this.location = location;
+    }
+    return Block;
+}());
+var Entity = (function () {
+    function Entity(block) {
+        this.block = block;
+    }
+    Entity.prototype.getBounds = function () {
+    };
+    return Entity;
+}());
--- main.ts
+++ main.ts
@@ -3,10 +3,34 @@
 function init() {
 	var stage = new createjs.Stage("demoCanvas");
 
-	var circle = new createjs.Shape();
-	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-	circle.x = 100;
-	circle.y = 100;
-	stage.addChild(circle);
+	var rectangle = new createjs.Shape();
+	rectangle.graphics.beginFill("DeepSkyBlue").drawRoundRect(0, 0, 100, 100, 2);
+	rectangle.x = 100;
+	rectangle.y = 100;
+	stage.addChild(rectangle);
 	stage.update();
+}
+
+interface Bounds {
+	width: number;
+	height: number;
+}
+
+interface Location {
+	x: number;
+	y: number;
+}
+
+class Block {
+	constructor(public location: Location) {
+	}
+}
+
+class Entity {
+	constructor(public block: Block[]) {
+	}
+
+	getBounds(): Bounds{
+		
+	}
 }
