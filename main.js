be40319c7af89252c9dc0c8460d06e61e115325b
Betsegaw Tadele <betsegaw.ta@gmail.com>
2016-09-03 04:39:11 -0700

    Add linq library

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 /// <reference path="DefinitelyTyped\createjs\createjs.d.ts" />
+/// <reference path="DefinitelyTyped\linq\linq.d.ts" />
 function init() {
+    testGraphics();
+}
+function testLinq() {
+    var entity = new Entity([new Block(new Loc(0, 0)), new Block(new Loc(10, 10)), new Block(new Loc(20, 20)), new Block(new Loc(30, 30))]);
+    return entity.getBounds();
+}
+function testGraphics() {
     var stage = new createjs.Stage("demoCanvas");
     var rectangle = new createjs.Shape();
     rectangle.graphics.beginFill("DeepSkyBlue").drawRoundRect(0, 0, 100, 100, 2);
@@ -8,6 +16,20 @@ function init() {
     stage.addChild(rectangle);
     stage.update();
 }
+var Bounds = (function () {
+    function Bounds(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    return Bounds;
+}());
+var Loc = (function () {
+    function Loc(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    return Loc;
+}());
 var Block = (function () {
     function Block(location) {
         this.location = location;
@@ -19,6 +41,9 @@ var Entity = (function () {
         this.block = block;
     }
     Entity.prototype.getBounds = function () {
+        var width = Enumerable.From(this.block).Max("$.location.x") - Enumerable.From(this.block).Min("$.location.x");
+        var height = Enumerable.From(this.block).Max("$.location.y") - Enumerable.From(this.block).Min("$.location.y");
+        return new Bounds(width, height);
     };
     return Entity;
 }());
--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 /// <reference path="DefinitelyTyped\createjs\createjs.d.ts" />
+/// <reference path="DefinitelyTyped\linq\linq.d.ts" />
 
 function init() {
+	testGraphics();
+}
+
+function testLinq() : Bounds {
+	var entity = new Entity([new Block(new Loc(0,0)), new Block(new Loc(10,10)), new Block(new Loc(20,20)), new Block(new Loc(30,30))]);
+	return entity.getBounds();
+}
+
+function testGraphics() {
 	var stage = new createjs.Stage("demoCanvas");
 
 	var rectangle = new createjs.Shape();
@@ -11,18 +21,20 @@ function init() {
 	stage.update();
 }
 
-interface Bounds {
-	width: number;
-	height: number;
+class Bounds {
+	constructor(public width: number, public height: number) {
+
+	}
 }
 
-interface Location {
-	x: number;
-	y: number;
+class Loc {
+	constructor (public x: number, public y: number){
+
+	}
 }
 
 class Block {
-	constructor(public location: Location) {
+	constructor(public location: Loc) {
 	}
 }
 
@@ -30,7 +42,10 @@ class Entity {
 	constructor(public block: Block[]) {
 	}
 
-	getBounds(): Bounds{
-		
+	getBounds(): Bounds {
+		var width = Enumerable.From(this.block).Max("$.location.x") - Enumerable.From(this.block).Min("$.location.x");
+		var height = Enumerable.From(this.block).Max("$.location.y") - Enumerable.From(this.block).Min("$.location.y");
+
+		return new Bounds(width, height);
 	}
 }
